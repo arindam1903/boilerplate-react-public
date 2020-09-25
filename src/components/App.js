ad0e49a44ed0cb1981e82534a858079eb11fd457
Ritesh Ranjan <unsortedarray@Riteshs-MacBook-Pro.local>
2020-09-26 00:33:58 +0530

    updated react boiler-plate

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,11 @@ import InputButton from "./InputButton";
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          count: 0
-        };
-        const udpateValue = this.udpateValue.bind(this);
-      }
-    udpateValue() {
-        this.setState({
-            count: this.state.count+1
-        })
-    }
     render() {
 
         return(
             <>
-                <CounterNumber count= {this.state.count} />
-                <InputButton  setCount = {() => {this.udpateValue(this.state.count+1)}}/>
+               
             </>
         )
     }
