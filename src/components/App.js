2e35b7fc13be124046494ae66129e009260aa1b7
Ritesh <ricksrv.5@gmail.com>
2020-09-26 14:07:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,5 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-import CounterNumber from "./CounterNumber";
-import InputButton from "./InputButton";
-
 
 class App extends Component {
     render() {
